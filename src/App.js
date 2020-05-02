3d21098f03df9146f4e426af4968d00a21efd6cf
xueqian-Destop <345589726@qq.com>
2020-05-02 23:02:44 +0800

    add react-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import Login from './view/login/login'
-import {Button} from 'antd'
-import 'antd/dist/antd.css'; 
-import {Route, Link, BrowserRouter} from 'react-router-dom'
+import React from "react";
+import Login from "./view/login/login";
+// import {Button} from 'antd'
+import "antd/dist/antd.css";
+import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
 function App() {
-  return (
-    <BrowserRouter>
-      <Route path="/home" render={()=>{
-        <h1>home page</h1>
-      }}/>
-      <Route render={()=><h1>没有匹配的</h1>} />
-
-    </BrowserRouter>
-
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route
+					path="/home"
+					render={() => {
+						return <h1>home page</h1>;
+					}}
+				/>
+        <Route path="/login" render={()=><h1>Login</h1>}/>
+				<Route render={() => <h1>没有匹配的</h1>} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
--- src/view/login/login.js
+++ src/view/login/login.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
-import './login.scss';
-export default class componentName extends Component {
-  render() {
-    return (
-      <div className="box">
-        <h1>你好</h1>
-      </div>
-    )
-  }
-}
+// import React, { Component } from 'react'
+// import './login.scss';
+// export default class componentName extends Component {
+//   render() {
+//     return (
+//       <div className="box">
+//         <h1>你好</h1>
+//       </div>
+//     )
+//   }
+// }
