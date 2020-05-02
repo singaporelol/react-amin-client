a8e846aff558ea41dc08b6d1b7bae9ead128d1f5
xueqian-Destop <345589726@qq.com>
2020-05-02 22:51:04 +0800

    setup environment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Login from './view/login/login'
+import {Button} from 'antd'
+import 'antd/dist/antd.css'; 
+import {Route, Link, BrowserRouter} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route path="/home" render={()=>{
+        <h1>home page</h1>
+      }}/>
+      <Route render={()=><h1>没有匹配的</h1>} />
+
+    </BrowserRouter>
+
   );
 }
 
